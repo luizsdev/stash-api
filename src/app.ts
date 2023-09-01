4566350858eecfa8ce9649eedaa2e03b459309fd
Luiz Sanches <luizsdev@gmail.com>
2023-08-31 22:06:20 -0300

    ✨ feat(app.ts): create a new file to handle the application logic and routes for user creation

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import { UserController } from "./adapters/controllers/User.controller";
+import { UserUseCaseImpl } from "./core/application/ports/usecases/CreateUser.usecase";
+import { UserRepositoryImpl } from "./adapters/repositories/user.repository.implementation";
+const app = express();
+
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+const repository = new UserRepositoryImpl();
+const userUseCase = new UserUseCaseImpl(repository);
+const userController = new UserController(userUseCase);
+app.post("/user", async (req, res) => {
+  await userController.createUser(req, res);
+});
