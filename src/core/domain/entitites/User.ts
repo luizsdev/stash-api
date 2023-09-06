1f08c528c779dd40f085a5e19dd928a9ca07688c
Luiz Sanches <luizsdev@gmail.com>
2023-09-05 21:01:16 -0300

    🔒 chore(User.ts): refactor User class to use async/await for password hashing and rename userName to username for consistency 🔒 chore(User.ts): add private static method hashPassword to hash user passwords using bcrypt for improved security

--- src/core/domain/entitites/User.ts
+++ src/core/domain/entitites/User.ts
@@ -1,26 +1,32 @@
+import bcrypt from "bcrypt";
 export class User {
   constructor(
-    private userName: string,
+    private username: string,
     private passwordHash: string,
     private fullName: string
   ) {}
 
-  static createNewUser(
-    userName: string,
+  static async createNewUser(
+    username: string,
     password: string,
     fullName: string
-  ): User {
-    const passwordHash = password; // TODO: hash password
-    return new User(userName, passwordHash, fullName);
+  ): Promise<User> {
+    const passwordHash = await this.hashPassword(password);
+    return new User(username, passwordHash, fullName);
   }
 
-  getUserName(): string {
-    return this.userName;
+  getUsername(): string {
+    return this.username;
   }
   getPasswordHash(): string {
     return this.passwordHash;
   }
   getFullName(): string {
     return this.fullName;
+  }
+
+  private static async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
   }
 }
